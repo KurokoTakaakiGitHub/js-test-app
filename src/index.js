8bcb71e5450ae5068d58c512ad82936b3b148c59
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-02-23 13:24:26 +0900

    js-learning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-import "./styles.css";
+/**
+ * テンプレート文字列
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const name = "太郎";
+// const age = 18;
+// const message = `自分の名前は${name}です。年齢は${age}です`;
+
+// console.log(message);
+
+/**
+ * アロー関数
+ */
+
+// 従来の書き方
+const func1 = function (str) {
+  return str;
+};
+
+console.log(func1("test1"));
+
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("test2"));
+
+const func3 = (str) => str;
+
+console.log(func3("test3"));
+
+/**
+ * 分割代入
+ */
+
+const myProfile = {
+  name: "太郎",
+  age: 20
+};
+
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}です`;
+console.log(message2);
+
+/**
+ * デフォルト値
+ */
+
+const syHello = (name) => console.log(`こんにちは!${name}さん!`);
+syHello();
